b2f789acc0878711777f3b6664b7ae3cf96a6944
animes-barman <animesbarman101@gmail.com>
2024-03-24 23:52:20 +0600

    setup all link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
 import './App.css'
+import { Alert } from "@material-tailwind/react";
+
 
 function App() {
 
   return (
-    <div>helolo</div>
+    <div>
+      <Alert>A simple alert for showing message.</Alert>
+    </div>
   )
 }
 
